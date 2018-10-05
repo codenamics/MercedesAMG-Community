48322c4a6402a8b12407f34d847fb4046bb64144
codenamics <info@codenamics.pl>
2018-10-05 22:52:19 +0200

    user model

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
 const app = express();
 //DB config
 const db = require('./config/keys').mongoURI
@@ -10,6 +14,11 @@ const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log('Server running')
 })
+
+//Use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 //Connect to mongoDB
 mongoose
