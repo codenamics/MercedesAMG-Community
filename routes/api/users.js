a30ad98424757c3976d8233453a88ec8165a3e43
codenamics <info@codenamics.pl>
2018-10-08 22:05:39 +0200

    jwt, validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,14 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
+const passport = require('passport')
 //Models
 const User = require('../../models/User')
+
+//Validation
+const validateRegisterInput = require('../../validation/register')
 
 
 // @route   GET api/users/test
@@ -17,15 +23,21 @@ router.get('/test', (req, res) => res.json({
 // @desc    Register
 // @access  Public
 router.post('/register', (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body)
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
     User.findOne({
             email: req.body.email
         })
         .then(
             user => {
                 if (user) {
-                    return res.status(400).json({
-                        email: 'Email already exists'
-                    })
+                    errors.email = 'Email already exists'
+                    return res.status(400).json(errors)
                 } else {
                     const avatar = gravatar.url(req.body.email, {
                         s: '200',
@@ -76,8 +88,18 @@ router.post('/login', (req, res) => {
             .then(
                 isMatch => {
                     if (isMatch) {
-                        res.json({
-                            msg: 'Success'
+                        const payload = {
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar
+                        }
+                        jwt.sign(payload, keys.secretOrKey, {
+                            expiresIn: 3600
+                        }, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: 'Bearer ' + token
+                            })
                         })
                     } else {
                         return res.status(400).json({
@@ -88,6 +110,20 @@ router.post('/login', (req, res) => {
             )
     })
 
+})
+
+// @route   GET api/users/current
+// @desc    Current user
+// @access  Private
+
+router.get('/current', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    })
 })
 
 
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser')
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
+const passport = require('passport')
 
 const app = express();
+
 app.use(bodyParser.urlencoded({
     extended: false
 }))
@@ -18,6 +20,11 @@ const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log('Server running')
 })
+//Passport
+app.use(passport.initialize())
+
+//Passport config
+require('./config/passport.js')(passport)
 
 //Use routes
 app.use('/api/users', users)
